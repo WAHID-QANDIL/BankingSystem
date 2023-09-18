d7e6e03d4de1ab7645f3ffcb0ad21ef5f9bc2a33
Wahid Ali <1213089@dtu.edu.eg>
2023-09-18 16:25:14 +0300

    Upgrade program && add new functions
    Enable write and read on files and store data on files
    That is make program data stored

---
+++ Banking system/Banking system.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include"Database.h"
+#include"Operations.h"
+#include"Clint.h"
+using namespace std;
+using namespace Database;
+int main()
+{
+	OP::startProgram();
+	return 0;
+}
